0ad73b3e5bdb49f4c57e2b956e47297416eb212e
Zackeus Zetterberg <zackeua@gmail.com>
2023-04-26 22:37:12 +0200

    Replaced lists with arrays to improve speed. Fore some reason process gets killed.

--- src/haskell/brainfuck.hs
+++ src/haskell/brainfuck.hs
@@ -1,5 +1,6 @@
 import System.Environment ( getArgs )
 import Data.List
+import Data.Array
 
 import Data.Map ( (!), empty, insert, member, Map, toList )
 
@@ -7,37 +8,37 @@ import Data.Char
 
 import System.IO
 
-run :: [Char] -> Int -> Map Int Int -> Int -> IO ()
+run :: Array Int Char -> Int -> Map Int Int -> Int -> IO ()
 run ops ip dat dp | ip >= length ops || ip < 0 = do
                                         return ()
-                  | ops!!ip == '>' = run ops (ip+1) dat (dp+1)
-                  | ops!!ip == '<' = run ops (ip+1) dat (dp-1)
-                  | ops!!ip == '+' = run ops (ip+1) (Data.Map.insert dp (if Data.Map.member dp dat then dat ! dp + 1 else 1) dat) dp
-                  | ops!!ip == '-' = run ops (ip+1) (Data.Map.insert dp (if Data.Map.member dp dat then dat ! dp - 1 else (-1)) dat) dp
-                  | ops!!ip == '.' = do
-                                        if Data.Map.member dp dat then putChar (chr (dat ! dp)) else putChar 'c'
+                  | ops Data.Array.! ip == '>' = run ops (ip+1) dat (dp+1)
+                  | ops Data.Array.! ip == '<' = run ops (ip+1) dat (dp-1)
+                  | ops Data.Array.! ip == '+' = run ops (ip+1) (Data.Map.insert dp (if Data.Map.member dp dat then dat Data.Map.! dp + 1 else 1) dat) dp
+                  | ops Data.Array.! ip == '-' = run ops (ip+1) (Data.Map.insert dp (if Data.Map.member dp dat then dat Data.Map.! dp - 1 else (-1)) dat) dp
+                  | ops Data.Array.! ip == '.' = do
+                                        if Data.Map.member dp dat then putChar (chr (dat Data.Map.! dp)) else putChar 'c'
                                         run ops (ip+1) dat dp
-                  | ops!!ip == ',' = do
+                  | ops Data.Array.! ip == ',' = do
                                     ch <- getChar
                                     let num = ord ch
                                     run ops (ip+1) (Data.Map.insert dp num dat) dp
-                  | ops!!ip == '[' = if not (Data.Map.member dp dat) || dat ! dp == 0 then do
+                  | ops Data.Array.! ip == '[' = if not (Data.Map.member dp dat) || dat Data.Map.! dp == 0 then do
                                                                                             let jmp = getJumpForward ops (ip+1) 1
                                                                                             run ops (jmp+1) dat dp
                                                                                       else run ops (ip+1) dat dp
 -- otherwise == ops!!ip == ']'
-                  | otherwise  = if Data.Map.member dp dat && dat ! dp /= 0 then do
+                  | otherwise  = if Data.Map.member dp dat && dat Data.Map.! dp /= 0 then do
                                                                     let jmp = getJumpBackward ops (ip-1) (-1)
                                                                     run ops (jmp+1) dat dp
                                                                 else run ops (ip+1) dat dp
 
-getJumpForward :: [Char] -> Int -> Int -> Int 
+getJumpForward :: Array Int Char -> Int -> Int -> Int 
 getJumpForward ops ip count 
-                            | count /= 0 = if ops !! ip == '[' then getJumpForward ops (ip+1) (count+1) else if ops !! ip == ']' then getJumpForward ops (ip+1) (count-1) else getJumpForward ops (ip+1) count
+                            | count /= 0 = if ops Data.Array.! ip == '[' then getJumpForward ops (ip+1) (count+1) else if ops Data.Array.! ip == ']' then getJumpForward ops (ip+1) (count-1) else getJumpForward ops (ip+1) count
                             | otherwise  = ip-1
-getJumpBackward :: [Char] -> Int -> Int -> Int 
+getJumpBackward :: Array Int Char -> Int -> Int -> Int 
 getJumpBackward ops ip count 
-                             | count /= 0 = if ops !! ip == '[' then getJumpBackward ops (ip-1) (count+1) else if ops !! ip == ']' then getJumpBackward ops (ip-1) (count-1) else getJumpBackward ops (ip-1) count
+                             | count /= 0 = if ops Data.Array.! ip == '[' then getJumpBackward ops (ip-1) (count+1) else if ops Data.Array.! ip == ']' then getJumpBackward ops (ip-1) (count-1) else getJumpBackward ops (ip-1) count
                              | otherwise  = ip+1
 
 main :: IO ()
@@ -46,5 +47,6 @@ main = do
     instream <- readFile (head args)
     let instructions = Data.List.filter (`elem` "<>+-.,[]") instream
     --print instructions
-    run instructions 0 Data.Map.empty 0
+    let array_instructions = array (0, length instructions) [(i, instructions !! i) | i <- [0..length instructions]]
+    run array_instructions 0 Data.Map.empty 0
     
